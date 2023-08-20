ea7f55f46c6d9650f310b55e3018c240bfc6abb0
modcoders <142755696+modcoders@users.noreply.github.com>
2023-08-20 18:31:20 -0400

    Add files via upload

---
+++ ModCodersFinalWebsite/assets/js/script.js
@@ -0,0 +1,52 @@
+
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navbarToggler = document.querySelector("[data-nav-toggler]");
+
+navbarToggler.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+});
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.remove("active");
+    navbarToggler.classList.remove("active");
+  });
+}
+
+
+
+/**
+ * search toggle
+ */
+
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+const searchBox = document.querySelector("[data-search-box]");
+
+for (let i = 0; i < searchTogglers.length; i++) {
+  searchTogglers[i].addEventListener("click", function () {
+    searchBox.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * subscribe
+ */
+function validateForm(){
+  let email = document.forms["Subscribe"]["email_address"];
+
+  if (email.validity.valueMissing){
+    alert("Enter your email!")
+  }
+}
+
+var day_description = "Halloween";
+var day_before = "Halloween Eve";
+
+var today = new Date();
+var year = today.getYear();
+
+
